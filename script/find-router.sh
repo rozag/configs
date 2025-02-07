aceadccd37db92bb7f173d101092c357bbaa1807
Alexey Mileev <alemileev@gmail.com>
2025-02-07 15:29:26 +0400

    Make the find router script search for actual Keenetic router

--- script/find-router.sh
+++ script/find-router.sh
@@ -8,6 +8,18 @@ ping_ip() {
     return $?
 }
 
+# Function to send an HTTP GET request and check for Keenetic HTML page
+check_keenetic() {
+    local ip=$1
+    # Send HTTP GET request with timeout of 60 seconds and check for Keenetic HTML page
+    response=$(curl -s -m 60 $ip)
+    if [[ $response == *"KeeneticOS Web Panel"* ]]; then
+        echo $ip
+        return 0
+    fi
+    return 1
+}
+
 # Loop through IP addresses
 for i in {1..254}; do
     # Construct the IP address
@@ -16,20 +28,20 @@ for i in {1..254}; do
     # Ping the IP address in the background
     (
         if ping_ip $ip; then
-            echo $ip
+            if check_keenetic "http://$ip"; then
+                exit 0
+            fi
         fi
     ) &
-
-    # # Construct the IP address
-    # ip="192.168.1.$i"
-    #
-    # # Ping the IP address in the background
-    # (
-    #     if ping_ip $ip; then
-    #         echo $ip
-    #     fi
-    # ) &
 done
 
 # Wait for all background processes to complete
 wait
+
+# Check if any background process found the Keenetic router
+if [[ $? -eq 0 ]]; then
+    exit 0
+else
+    echo "Keenetic router not found on the network."
+    exit 1
+fi
